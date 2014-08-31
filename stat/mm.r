9792ec3e18779a66a232d5f450143733de9b7ed5
Alex Carlin <carlin@ucdavis.edu>
2014-08-31 02:16:11 -0700

    Cleaned up

--- stat/mm.r
+++ stat/mm.r
@@ -1,20 +1,18 @@
-argv <- commandArgs(trailingOnly = TRUE)
-r=read.csv(argv[1], header=T)
+#argv=commandArgs(trailingOnly = TRUE)
+#r=read.csv(argv[1], header=T)
+r=read.csv('example.csv')
 s=split(r,r$Sample)
 
-library(nls2)
-print('Michaelis-Menten models')
-lapply(s, function(x) {
+michael=function(x) {
   b=c(kcat=max(x$kobs),km=mean(x$S))
-  try(coef(nls(kobs~kcat*S/(km+S),x,start=b)))
-})
+  try(nls(kobs~kcat*S/(km+S),x,start=b))
+}
+
+print('Michaelis-Menten models')
+lapply(s, function(x) { summary(michael(x)) })
 
 print('Linear fits')
-lapply(s, function(x) { 
-  coef(lm(kobs~S,x))
-})
-
-q()
+lapply(s, function(x) { summary(lm(kobs~S,x)) })
 
 library(ggplot2)
 ggplot(r, aes(S, kobs)) + 
