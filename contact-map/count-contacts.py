a74620c8fe5c9a021c8c0e83aad9fa16938d2c43
Alex Carlin <carlin@ucdavis.edu>
2014-11-07 14:40:55 -0800

    Added Python plots for complied data

--- contact-map/count-contacts.py
+++ contact-map/count-contacts.py
@@ -1,19 +1,49 @@
+# pdb parser 
+
 from re import match
 from numpy import array 
 
-class CA:
-  def __init__(self, inp):
-    self.aa = inp[17:20]
+class Atom:
+  def __init__(self, l):
+    self.record = l[0:6]
+    self.atom_serial = int(l[6:11]) 
+    self.atom_n = int(l[12:16])
+    self.alt_loc = l[16]
+    self.aa = l[17:20]
+    self.chain = l[21] 
+    self.aa_n = int(l[22:26])
+    self.achar = l[26]
     self.xyz = (float(inp[31:38]), float(inp[40:46]), float(inp[48:54]))
+    self.record = l[0:6]
+
+    self.attr = { 
+      'record': l[0:6].strip(),
+      'atom_serial': int(l[6:11]), 
+      'atom_n': int(l[12:16]),
+      'alt_loc': l[16],
+      'aa': l[17:20],
+      'chain': l[21],
+      'aa_n': int(l[22:26]),
+      'achar': l[26],
+      'xyz': (float(inp[31:38]), float(inp[40:46]), float(inp[48:54]))
+      'occupancy': float(l[54:60]),
+      'temp_factor': float(l[60:66]),
+      'seg_id': l[72:76].strip(),
+      'element': l[76:78].strip(),
+      'charge': l[78:80].strip(),
+    }
 
   def dist(self, other):
     x1, y1, z1 = self.xyz
     x2, y2, z2 = other.xyz
     return ((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)**0.5
 
-with open('../foldit/bagel.pdb') as fn:
-  seq = [ CA(l) for l in fn if match(r'^AT.+CA', l) ]
+class PDB:
+  'Represents a single PDB, use like 1sny = PDB("1sny.pdb")'
+  def __init__(self, fn):
+    with open(fn) as fn:
+      self.atoms = [ Atom(l) for l in fn if match(r'^ATOM', l) ]
+      self.hetatms = [ Atom(l) for l in fn if match(r'^HETATOM', l) ] 
 
-_map = [ [ (1, ca.aa) if ca.dist(i) < 10 else 0 for ca in seq ] for i in seq ]
-_matrix = array(_map)
-print(_map)
+#contact_map = [ [ (1, ca.aa) if ca.dist(i) < 10 else 0 for ca in cas ] for i in seq ]
+#_matrix = array(_map)
--- stat/guess-constants.r
+++ stat/guess-constants.r
@@ -7,20 +7,25 @@ michael=function(x) {
   try(summary(nls(kobs~kcat*S/(km+S),x,start=b)))
 }
 
+si=function(x) {
+  b=c(kcat=max(x$kobs),km=mean(x$S),ki=km)
+  try(summary(nls(kobs~kcat*S/(km+S+S^2/ki),x,start=b)))
+}
+
 lapply(s, function(x) { michael(x) })
 
 print('---666---')
 
 lapply(s, function(x) { try(summary(lm(kobs~S,x))) })
 
-#library(ggplot2)
-#ggplot(r, aes(S, kobs)) + 
-#theme_minimal() + 
-#geom_point() + 
-#stat_smooth(method="lm", se=F, color="green") + 
-#facet_wrap(~Sample,scales="free") +
-#geom_smooth(method="nls",
-#formula=y~kcat*x/(km+x),
-#start=c(kcat=1,km=0.01),se=F) + 
-#ggsave(paste(argv[1] ".pdf"))
+library(ggplot2)
+ggplot(r, aes(S, kobs)) + 
+theme_minimal() + 
+geom_point() + 
+stat_smooth(method="lm", se=F, color="green") + 
+facet_wrap(~Sample,scales="free") +
+geom_smooth(method="nls",
+formula=y~kcat*x/(km+2*x),
+start=c(kcat=1,km=0.01),se=F) + 
+ggsave("out.pdf")
 
