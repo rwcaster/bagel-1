81a51a86731ea81bca3bf6d01cdbb14c819cb268
Alex Carlin <carlin@ucdavis.edu>
2015-03-31 14:09:55 -0700

    Cleaned up

--- improve/out/cat_score.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import glob 
-all_scores = pandas.DataFrame() 
-for sf in glob.glob( '*sc' ):
-  df = pandas.read_csv( sf , sep=r'\s+', header=0 ) 
-  all_scores = all_scores.append( df ) 
-
-all_scores.to_csv( 'all.sc' ) 
- 
