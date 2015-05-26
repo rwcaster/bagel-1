d8683515fbc840af7b47679c8fee89ca8341be04
Alex Carlin <carlin@ucdavis.edu>
2015-05-26 15:29:40 -0700

    Final version submitted to JACS

---
+++ models/run.bash
@@ -0,0 +1,14 @@
+#$ -cwd 
+#$ -S /bin/bash
+#$ -e logs
+#$ -o logs
+#$ -p -4 
+#4 -N all_bagels 
+
+export ROSETTA_DB3=/share/tmp-data-1/siegellab/Rosetta/main/database
+export PATH=$PATH:/share/tmp-data-1/siegellab/Rosetta/main/source/bin
+
+TARGET=$( awk 'NR=="'${SGE_TASK_ID}'" { print $1 }' short_list.txt )
+NEW_RES=$( awk 'NR=="'${SGE_TASK_ID}'" { print $2 }' short_list.txt )
+
+rosetta_scripts.linuxgccrelease @ repack.flags -in:file:s bagel.pdb -parser:script_vars position=$TARGET new_residue=$NEW_RES 
