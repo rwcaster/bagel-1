d159fd6317e413acad309a31f49c024c2b0f4210
Alex Carlin <carlin@ucdavis.edu>
2015-06-27 17:51:38 -0700

    Whoops, added core lib that the script relies on

---
+++ orders/core/db/amino_acid.py
@@ -0,0 +1,36 @@
+def rc(seq):
+  c = [{'a':'t', 't':'a', 'g':'c', 'c':'g', 'A':'T', 'C':'G', 'T':'A', 'G':'C', }[i] for i in str(seq)] 
+  return ''.join(c)[::-1]
+
+aa = { 'ata':'i', 'atc':'i', 'att':'i', 'atg':'m', 'aca':'t', 'acc':'t', 'acg':'t', 'act':'t', 'aac':'n', 'aat':'n', 'aaa':'k', 'aag':'k', 'agc':'s', 'agt':'s', 'aga':'r', 'agg':'r', 'cta':'l', 'ctc':'l', 'ctg':'l', 'ctt':'l', 'cca':'p', 'ccc':'p', 'ccg':'p', 'cct':'p', 'cac':'h', 'cat':'h', 'caa':'q', 'cag':'q', 'cga':'r', 'cgc':'r', 'cgg':'r', 'cgt':'r', 'gta':'v', 'gtc':'v', 'gtg':'v', 'gtt':'v', 'gca':'a', 'gcc':'a', 'gcg':'a', 'gct':'a', 'gac':'d', 'gat':'d', 'gaa':'e', 'gag':'e', 'gga':'g', 'ggc':'g', 'ggg':'g', 'ggt':'g', 'tca':'s', 'tcc':'s', 'tcg':'s', 'tct':'s', 'ttc':'f', 'ttt':'f', 'tta':'l', 'ttg':'l', 'tac':'y', 'tat':'y', 'taa':'_', 'tag':'_', 'tgc':'c', 'tgt':'c', 'tga':'_', 'tgg':'w'} 
+
+ecoli_codon = { 'g':'ggc', 'a':'gcg', 'v':'gtg', 'f':'ttt', 'e':'gaa', 'd':'gat', 'n':'aac', 'h':'cat', 'p':'ccg', 'q':'cag', 'w':'tgg', 'y':'tat', 'i':'att', 'm':'atg', 'c':'tgc', 'k':'aaa', 'l':'ctg', 'r':'cgt', 't':'acc', 's':'agc'}
+
+aa1_upper = list("ACDEFGHIKLMNPQRSTVWY")
+aa1_lower = list("acdefghiklmnpqrstvwy")
+aa3_upper = "ALA CYS ASP GLU PHE GLY HIS ILE LYS LEU MET ASN PRO GLN ARG SER THR VAL TRP TYR".split()
+aa3_lower = "ala cys asp glu phe gly his ile lys leu met asn pro gln arg ser thr val trp tyr".split()
+
+def one_to_three(residue_as_1_letter_code):
+  return dict( zip(aa1_lower, aa3_lower) )[residue_as_1_letter_code]
+
+def one_to_THREE(residue_as_1_letter_code):
+  return dict( zip(aa1_lower, aa3_upper) )[residue_as_1_letter_code]
+  
+def ONE_to_three(residue_as_1_letter_code):
+  return dict( zip(aa1_upper, aa3_lower) )[residue_as_1_letter_code]
+
+def ONE_to_THREE(residue_as_1_letter_code):
+  return dict( zip(aa1_upper, aa3_upper) )[residue_as_1_letter_code]
+
+def three_to_one(aa):
+  return dict(zip(aa3_lower, aa1_lower))[aa]
+
+def three_to_ONE(aa):
+  return dict(zip(aa3_lower, aa1_upper))[aa]
+
+def THREE_to_one(aa):
+  return dict(zip(aa3_upper, aa1_lower))[aa]
+
+def THREE_to_ONE(aa):
+  return dict(zip(aa3_upper, aa1_upper))[aa]
